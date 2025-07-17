10311ac44100ec12e021b484e2202fbdfd997943
regina makena <128365780+ginakena@users.noreply.github.com>
2025-07-17 14:49:28 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,8 +13,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true, 
+  origin: "https://blogit-frontend-fq1q.onrender.com", 
+  credentials: true,
   methods: ["POST", "GET", "PATCH", "DELETE", "PUT"]
 }));
 
